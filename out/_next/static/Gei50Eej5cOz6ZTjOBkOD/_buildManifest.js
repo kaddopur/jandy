253d52215f5c66a8577daa3285245c27328ee34c
Jason Huang <chaoju.huang@gmail.com>
2022-02-14 17:27:27 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/Gei50Eej5cOz6ZTjOBkOD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-036ae90f36d31053.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],"/about":["static/chunks/pages/about-f3fa644573272d24.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
