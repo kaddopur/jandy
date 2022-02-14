45742e648843f40e86c71e4a9295039bd2880de3
Jason Huang <chaoju.huang@gmail.com>
2022-02-14 17:45:46 +0800

    feat: adjust breakpoint

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-[#4e100e] md:flex-row">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-[#4e100e] lg:flex-row">
       <img src="./invitation_01.jpg" width={520} height={725} />
       <img src="./invitation_02.jpg" width={520} height={725} />
     </div>
