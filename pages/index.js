84e0da78a94512032bf1ada39ea7e9539286c01d
Jason Huang <chaoju.huang@gmail.com>
2022-02-14 16:59:41 +0800

    feat: add invitaion images

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Link from 'next/link'
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
+    <div
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "1em",
+          backgroundColor: "#e7b98e",
+        }}
+      >
+        <Image src="/invitation_01.jpg" width={520} height={725} />
+        <Image src="/invitation_02.jpg" width={520} height={725} />
+      </div>
     </div>
-  )
+  );
 }
