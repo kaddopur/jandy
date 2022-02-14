1d0235fdc249d00dc107be0e0948cfa1f36ae588
Jason Huang <chaoju.huang@gmail.com>
2022-02-14 17:13:22 +0800

    feat: apply tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "1em",
-          backgroundColor: "#e7b98e",
-        }}
-      >
-        <Image src="/invitation_01.jpg" width={520} height={725} />
-        <Image src="/invitation_02.jpg" width={520} height={725} />
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-[#d7b98e] md:flex-row">
+      <Image src="/invitation_01.jpg" width={520} height={725} />
+      <Image src="/invitation_02.jpg" width={520} height={725} />
     </div>
   );
 }
