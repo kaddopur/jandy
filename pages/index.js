855f81228ad0bb14b713641ce89b537b5a86443e
Jason Huang <chaoju.huang@gmail.com>
2022-02-14 17:29:51 +0800

    fix: fix image path

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-[#d7b98e] md:flex-row">
-      <Image src="/invitation_01.jpg" width={520} height={725} />
-      <Image src="/invitation_02.jpg" width={520} height={725} />
+      <img src="./invitation_01.jpg" width={520} height={725} />
+      <img src="./invitation_02.jpg" width={520} height={725} />
     </div>
   );
 }
